e5d6e139ffff0aa7f0e47c7265c724b121b569c7
Borhan Darvishi <borhandr23@gmail.com>
2024-07-20 11:06:46 +0330

    x

--- Personal_Financial_Manager/data_entry.py
+++ Personal_Financial_Manager/data_entry.py
@@ -1,24 +1,37 @@
 from datetime import datetime
 
-def get_date(prompt,allows_default= False)
+date_format = "%d-%m-%Y"
+CATEGORIES = {"I":"Income","E": "Expense"}
+
+def get_date(prompt,allows_default= False):
     date_str = input(prompt)
     if allows_default and not date_str:
-        return datetime.today().strftime("%d-%m-%Y")
+        return datetime.today().strftime(date_format)
     
     try:
-        valid_date = datetime.strptime(date_str,"%d-%m-%Y")
-        return valid_date.strftime("%d-%m-%Y" )
+        valid_date = datetime.strptime(date_str,date_format)
+        return valid_date.strftime(date_format)
     except ValueError:
         print("Invalid date format. please enter date in dd-mm-yyyy")
-
+        return get_date(prompt,allows_default= False)
 
 
 def get_amount():
-    pass
-
+    
+    try:
+        amount = float(input("Enter the amount: "))
+        if amount <= 0:
+            raise ValueError("Amount must be a non-negative not-zero value.")
+        return amount
+    except ValueError as e:
+        print(e)
+        return get_amount()
 
 def get_category():
-    pass
+    category = input("Enter the category ('I' for Income or 'E' for Expense): ").upper()
+    if category in CATEGORIES:
+        return CATEGORIES[category]
+    print("Invalid category. please enter I' for Income or 'E' for Expense)")
 
 def get_description():
     pass
